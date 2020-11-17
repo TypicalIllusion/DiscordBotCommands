e61b40a03e9f16cfd31f123d81b3cd4b26132e5e
TypicalIllusion <70919038+TypicalIllusion@users.noreply.github.com>
2020-11-17 17:04:19 -0500

    Update mute6h.js

--- mute6h.js
+++ mute6h.js
@@ -6,7 +6,7 @@ module.exports= {
     let receiver = message.mentions.members.first();
     if(!message.mentions.members.first())
         return message.reply("Please use @ instead of copying the name.")
-    if(!message.member.roles.cache.has('753335690476912650')) 
+    if(!message.member.roles.cache.has('ROLE ID')) 
         return message.reply("You can't use this!")
     let member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(x => x.user.username === args.slice(0).join(" ") || x.user.username === args[0])
     if(member.hasPermission(['MANAGE_ROLES']) && !message.member.hasPermission('MANAGE_ROLES')) return;
